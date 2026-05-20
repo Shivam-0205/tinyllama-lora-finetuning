caa05721cb4369a08fbdbd06d891d1871fa0a808
Shivam-0205 <shivamvin@gmail.com>
2026-05-20 11:47:12 +0530

    Update inference.py

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,1 +1,26 @@
+from transformers import pipeline
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
+MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+tokenizer = AutoTokenizer.from_pretrained("./tinyllama-lora-model")
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME
+)
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer
+)
+
+prompt = "Machine learning is"
+
+result = pipe(
+    prompt,
+    max_new_tokens=50
+)
+
+print(result[0]["generated_text"])
