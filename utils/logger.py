3d815a2dbff0ca311747f4dd593c818edb715f1c
Shivam-0205 <shivamvin@gmail.com>
2026-05-20 12:05:52 +0530

    Add Weights & Biases logging functions

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,13 @@
+import wandb
 
+def initialize_wandb(project_name, run_name):
+    wandb.init(
+        project=project_name,
+        name=run_name
+    )
+
+def log_metrics(metrics):
+    wandb.log(metrics)
+
+def finish_wandb():
+    wandb.finish()
